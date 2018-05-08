14190dc467e5ea39f1747fc001bfd3702c5f4ebb
MASTER-PNZ <paulkeesling@gmail.com>
2018-05-08 12:54:28 -0400

    adding comments for structure

--- js/main.js
+++ js/main.js
@@ -2,7 +2,38 @@
 
 !function() {
 
-  
+//constants variables for page divs
+
+const startPage =
+<div class="screen screen-start" id="start">
+  <header>
+    <h1>Tic Tac Toe</h1>
+    <a href="#" class="button">Start game</a>
+  </header>
+</div>;
+
+const gamePage =
+
+const gameEndPage =
+<div class="screen screen-win" id="finish">
+  <header>
+    <h1>Tic Tac Toe</h1>
+    <p class="message"></p>
+    <a href="#" class="button">New game</a>
+  </header>
+</div>;
+
+// Classes for Players and their moves
+
+// Game Board array class
+
+// function for game end state win or draw
+
+// function for buttons to add and name inputs
+
+// function for computer player logic
+
+//
 
 
 }();
