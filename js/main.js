2cb3a79505ad9dcce68c0ff5a2db32bd9392cac9
MASTER-PNZ <paulkeesling@gmail.com>
2018-05-21 15:01:10 -0400

    mouseover/out functionality
    started on user box fill click handler

--- js/main.js
+++ js/main.js
@@ -1,43 +1,67 @@
 // An IIFE to encapsulate the Tic-Tac-Toe Program in module format.
 
 !function() {
-
 //constants variables for page divs
-
 const startPage = $('#start');
 const gamePage = $('#board');
 const gameEndPage = $('#finish');
+const userInput = $('#user_name');
+const boardSqrs = $('.boxes');
+const oPlayer = $('#player1');
+const xPlayer = $('#player2');
+const xPlayerDiv = `<div class="player2Name" style="color:black;">Computer Player</div>`;
+let currentPlayer = oPlayer;
 
 $(function(){
     gamePage.hide();
     gameEndPage.hide();
 });
 
-$('#user_name').focus();
+userInput.focus();
 //function to start game and validate user input.
 
 $('.button').on("click", function (){
-  const player1Name =  $('#user_name').val();
+  let player1Name =  userInput.val();
+  let oPlayerDiv =`<div class="player1Name" style="color:black;">${player1Name}</div>`;
   if (player1Name == ""){
+    userInput.css('border-color', 'red');
     return false;
   } else if (player1Name.length > 0){
   startPage.hide();
   gamePage.show();
-  $('#player1').after(`<div class="player1Name" style="color:black;">${player1Name}</div>`);
+  }
+  oPlayer.after(oPlayerDiv);
+
+});
+
+currentPlayer.addClass('active');
+
+// Background Hover function
+
+boardSqrs.mouseover(function(event){
+  if(currentPlayer == oPlayer){
+    event.target.style.backgroundImage = 'url("./img/o.svg")';
+  } else if (currentPlayer == xPlayer) {
+    event.target.style.backgroundImage = 'url("./img/x.svg")';
   }
 });
 
-$('#player1').addClass("active");
+boardSqrs.mouseout(function(event){
+  if(currentPlayer.hasClass('active')){
+    event.target.style.backgroundImage = '';
+  }
+});
 
 
-
-// variables for html player elements
 
 // Game play functionality
 
 function gamePlay(){
-
-
+  if(currentPlayer == oPlayer) {
+    boardSqrs.on('click', function(event){
+      event.target.addClass('box-filled-1');
+    })
+  }
 }
 
 // Classes for Players and their moves
@@ -62,11 +86,7 @@ function endGameState () {
 
 }
 
-// function for box hover property
 
-// function for buttons to add and name inputs
-
-function button () {}
 
 // function for computer player logic
 
