ed18dd188b2f76392b41916bc3e93b7c635e2cad
MASTER-PNZ <paulkeesling@gmail.com>
2018-05-21 23:33:51 -0400

    Added a draw page
    working to add the winning pages. Need to set up board array for winning combinations.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 const startPage = $('#start');
 const gamePage = $('#board');
 const gameEndPage = $('#finish');
+const gameEndMessage = $('.message');
 const userInput = $('#user_name');
 const boardSqrs = $('.boxes');
 const boardSqr = $('.box');
@@ -12,8 +13,9 @@ const oPlayer = $('#player1');
 const xPlayer = $('#player2');
 const xPlayerDiv = `<div class="player2Name" style="color:black;">Computer Player</div>`;
 let currentPlayer = oPlayer;
-let gameMove = 0;
+let gameMoves = 0;
 const winBoard = [];
+let currentboard = [];
 
 $(function(){
     gamePage.hide();
@@ -26,21 +28,19 @@ userInput.focus();
 $('.button').on("click", function (){
   let player1Name =  userInput.val();
   let oPlayerDiv =`<div class="player1Name" style="color:black;">${player1Name}</div>`;
-  if (player1Name == ""){
+  if (player1Name == "") {
     userInput.css('border-color', 'red');
     return false;
-  } else if (player1Name.length > 0){
-  startPage.hide();
-  gamePage.show();
+  } else if (player1Name.length > 0) {
+      startPage.hide();
+      gamePage.show();
   }
   oPlayer.after(oPlayerDiv);
   //setting user player to "O".
   oPlayer.addClass('active');
 });
 
-
-
-// Background Hover functions
+// Background-Image Hover functions for board squares.
 
 boardSqr.mouseover(function(){
   if($(this).hasClass('disabled')){
@@ -61,27 +61,41 @@ boardSqr.mouseout(function(){
 
 // Game play function for player1 - user.
 boardSqr.click(function(){
+
   if(currentPlayer == oPlayer){
     $(this).addClass('box-filled-1 disabled');
     currentPlayer = xPlayer;
     oPlayer.removeClass('active');
     xPlayer.addClass('active');
+    gameMoves += 1;
   } else if (currentPlayer == xPlayer) {
     $(this).addClass('box-filled-2 disabled');
     currentPlayer = oPlayer;
     xPlayer.removeClass('active');
     oPlayer.addClass('active');
+    gameMoves += 1;
   }
+  endGameState();
 });
-
-// Game play function for computer turn.
 
 // function for game end state win or draw
 
 function endGameState () {
+  if(gameMoves == 9){
+    startPage.hide();
+    gamePage.hide();
+    gameEndPage.addClass('screen-win-tie');
+    gameEndMessage.text(`It's a Tie!`);
+    gameEndPage.show();
+  } else if(){
 
+  }
 }
 
+// When user clicks button the page is refreshed for the next game.
+$('.button:contains("New game")').click(function(){
+  window.location.reload(true);
+});
 
 
 }();
