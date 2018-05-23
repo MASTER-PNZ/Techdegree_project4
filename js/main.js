68d4606ec0d822277225b322d69f6b53dde27b90
MASTER-PNZ <paulkeesling@gmail.com>
2018-05-23 12:40:10 -0400

    game play complete
    functionality for game play against User and AI complete. Commencing programming for evaluting winning board states.

--- js/main.js
+++ js/main.js
@@ -11,18 +11,32 @@ const boardSqrs = $('.boxes');
 const boardSqr = $('.box');
 const oPlayer = $('#player1');
 const xPlayer = $('#player2');
-const xPlayerDiv = `<div class="player2Name" style="color:black;">Computer Player</div>`;
 let currentPlayer = oPlayer;
 let gameMoves = 0;
-const winBoard = [];
-let currentboard = [];
+let randomSqr;
+const winBoard = [
+  // winning rows
+  [boardSqr[0], boardSqr[1], boardSqr[2]],
+  [boardSqr[3], boardSqr[4], boardSqr[5]],
+  [boardSqr[6], boardSqr[7], boardSqr[8]],
+  // winning columns
+  [boardSqr[0], boardSqr[3], boardSqr[6]],
+  [boardSqr[1], boardSqr[4], boardSqr[7]],
+  [boardSqr[2], boardSqr[5], boardSqr[8]],
+  // winning diagonals
+  [boardSqr[0], boardSqr[4], boardSqr[8]],
+  [boardSqr[2], boardSqr[4], boardSqr[6]]
+];
+let player1Board = [];
+let player2Board = [];
 
 $(function(){
     gamePage.hide();
     gameEndPage.hide();
 });
-
+// adds focus to user input
 userInput.focus();
+
 //function to start game and validate user input.
 
 $('.button').on("click", function (){
@@ -59,37 +73,52 @@ boardSqr.mouseout(function(){
   }
 });
 
+let compMove = function() {
+  do {
+   randomSqr = Math.floor(Math.random() * 9);
+ } while ( currentPlayer == xPlayer && boardSqr.eq(randomSqr).hasClass('disabled') );
+   if (currentPlayer == xPlayer && !boardSqr.eq(randomSqr).hasClass('disabled')) {
+    boardSqr.eq(randomSqr).addClass('box-filled-2 disabled');
+    currentPlayer = oPlayer;
+    xPlayer.removeClass('active');
+    oPlayer.addClass('active');
+    gameMoves += 1;
+    player2Board.push(boardSqr.eq(randomSqr));
+  }
+}
 // Game play function for player1 - user.
 boardSqr.click(function(){
-
   if(currentPlayer == oPlayer){
     $(this).addClass('box-filled-1 disabled');
     currentPlayer = xPlayer;
     oPlayer.removeClass('active');
     xPlayer.addClass('active');
     gameMoves += 1;
-  } else if (currentPlayer == xPlayer) {
-    $(this).addClass('box-filled-2 disabled');
-    currentPlayer = oPlayer;
-    xPlayer.removeClass('active');
-    oPlayer.addClass('active');
-    gameMoves += 1;
+    player1Board.push($(this));
   }
+
+  setTimeout(compMove, 1500);
   endGameState();
 });
+
 
 // function for game end state win or draw
 
 function endGameState () {
   if(gameMoves == 9){
+    currentPlayer = '';
     startPage.hide();
     gamePage.hide();
     gameEndPage.addClass('screen-win-tie');
     gameEndMessage.text(`It's a Tie!`);
     gameEndPage.show();
-  } else if(){
-
-  }
+  } // else if (winBoard.length.hasClass('box-filled-1')){
+  //   gamePage.hide();
+  //   startPage.hide();
+  //   gameEndPage.addClass('screen-win-one');
+  //   gameEndMessage.text(`${player1Name} Wins!`);
+  //   gameEndPage.show();
+  // }
 }
 
 // When user clicks button the page is refreshed for the next game.
